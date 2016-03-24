6e73eb3e2952f1e95ed9c030f83426923a7acc36
Melissa Cabral <melissacabral@gmail.com>
2016-03-24 10:48:49 -0700

    adding the write post page :pencil: day 17

--- admin/admin-nav.php
+++ admin/admin-nav.php
@@ -1,12 +1,13 @@
 <nav role='navigation'>
 	<ul class="main">
 
-	<?php //if an administrator is logged in, show the full nav bar, otherwise show the commenter nav items
+	<?php //if an administrator is logged in, show the full nav bar, 
+	//otherwise show the commenter nav items
 	if( IS_ADMIN ){ 
 		//ADMINISTRATOR! ?>
 		
 		<li class="dashboard"><a href="index.php">Dashboard</a></li>
-		<li class="write"><a href="#">Write Post</a></li>
+		<li class="write"><a href="admin-write.php">Write Post</a></li>
 		<li class="edit"><a href="#">Edit Posts</a></li>
 		<li class="comments"><a href="#">Comments</a></li>
 		<li class="users"><a href="#">Manage Users</a></li>
